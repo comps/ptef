e332a1c3aa7e637fe2abd8b990341315037a328a
Jiri Jaburek <comps@nomail.dom>
2017-04-18 19:41:46 +0200

    relative path support, buggy

--- tef/runall.sh
+++ tef/runall.sh
@@ -2,22 +2,50 @@
 set -x
 
 TEF_ARGV0_NAME=$(basename "$0")
+TEF_ARGV0_DNAME=$(dirname "$0")
 TEF_ARGV=("$@")
-TEF_ARGI=0  #index
+
+tef_err() { echo "error: $1" 1>&2; exit 1; }
+#tef_warn() { echo "error: $1" 1>&2; }
 
 # if $1 is dir, run runner inside it, pass it args
 # if $1 is exec file, run it, pass it args
 tef_run_child() {
+    local args=("$@")
     if [ -d "$1" ]; then
         if [ -x "$1/$TEF_ARGV0_NAME" ]; then
             pushd "$1" >/dev/null
-            local args=("$@")
             args[0]="./$TEF_ARGV0_NAME"
             "${args[@]}"
             popd >/dev/null
         fi
     elif [ -x "$1" ]; then
-        "$@"
+        # not runnable without PATH
+        if [ "${1::1}" != "/" -a "${1::2}" != "./" ]; then
+            args[0]="./$1"
+        fi
+        "${args[@]}"
+    fi
+}
+
+# given an absolute path, return relative path from the runner's location
+# (essentially just omit the leading '/')
+# given a relative path from the caller's CWD, return path relative to
+# it from the runner's location
+tef_normalize_path() {
+    local path="$1"
+    if [ "${path::1}" = "/" ]; then
+        path=$(realpath -ms "$path")  # remove trailing '/', '//', '..', etc.
+        echo "${path:1}"  # omit leading '/'
+    else
+        realpath -ms --relative-to="$TEF_ARGV0_DNAME" "$path"
+#
+#        path=$(realpath -ms --relative-to="$TEF_ARGV0_DNAME" "$path")
+#        case "$path" in
+#            *..*)
+#            tef_warn "given path $path outside the runner scope, skipping" ;;
+#        esac
+#        echo "$path"
     fi
 }
 
@@ -35,19 +63,28 @@ tef_run() {
 
     # some args - run only the specified tests
     else
-        local i=0 arr= prefix= postfix=
-        while [ "$i" -lt "${#TEF_ARGV[@]}" ]; do
+        local argv=() arg= i=0 arr= prefix= postfix=
+
+        for arg in "${TEF_ARGV[@]}"; do
+            arg=$(tef_normalize_path "$arg")
+            case "$arg" in
+                *..*) tef_err "given path $path outside the runner scope" ;;
+            esac
+            argv+=("$arg")
+        done
+
+        while [ "$i" -lt "${#argv[@]}" ]; do
             arr=()
 
             # gather all args with a common prefix in sequential order,
             # store their postfixes (arg without prefix)
-            prefix="${TEF_ARGV[$i]%%/*}"
-            for (( ; i < "${#TEF_ARGV[@]}" ; i++ )); do
-                if [ "${TEF_ARGV[$i]%%/*}" = "$prefix" ]; then
-                    postfix="${TEF_ARGV[$i]#*/}"
+            prefix="${argv[$i]%%/*}"
+            for (( ; i < "${#argv[@]}" ; i++ )); do
+                if [ "${argv[$i]%%/*}" = "$prefix" ]; then
+                    postfix="${argv[$i]#*/}"
                     # if there's no '/', bash doesn't remove anything, so treat
                     # last path element as special (don't add anything to arr)
-                    if [ "$postfix" != "${TEF_ARGV[$i]}" ]; then
+                    if [ "$postfix" != "${argv[$i]}" ]; then
                         arr+=("$postfix")
                     fi
                 else
@@ -56,8 +93,8 @@ tef_run() {
             done
 
             arr=("$prefix" "${arr[@]}")
-            echo would run "${arr[@]}"
-            #tef_run_child "${arr[@]}"
+            #echo would run "${arr[@]}"
+            tef_run_child "${arr[@]}"
         done
     fi
 }
