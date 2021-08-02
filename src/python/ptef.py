d7b7ceeb95cc1dd1dde3c193a1d3141b695adbfa
Jiri Jaburek <comps@nomail.dom>
2021-08-03 00:12:37 +0200

    raise OSError in ptef.py when ptef_* return -1
    Signed-off-by: Jiri Jaburek <comps@nomail.dom>

--- src/python/ptef.py
+++ src/python/ptef.py
@@ -10,16 +10,29 @@ def runner(argv: list = None, basename: str = None, jobs: int = 1,
         basename = os.path.basename(sys.argv[0])
     argc = len(argv)
     argv_bstrings = (x.encode('utf-8') for x in argv)
-    libptef.ptef_runner(ctypes.c_int(argc),
-                        (ctypes.c_char_p * argc)(*argv_bstrings),
-                        ctypes.c_char_p(basename.encode('utf-8')),
-                        ctypes.c_int(jobs),
-                        ctypes.c_int(nomerge))
+    ctypes.set_errno(0)
+    rc = libptef.ptef_runner(ctypes.c_int(argc),
+                             (ctypes.c_char_p * argc)(*argv_bstrings),
+                             ctypes.c_char_p(basename.encode('utf-8')),
+                             ctypes.c_int(jobs),
+                             ctypes.c_int(nomerge))
+    if rc == -1:
+        errno = ctypes.get_errno()
+        raise OSError(errno, os.strerror(errno), None)
 
 def report(status: str, testname: str) -> None:
-    libptef.ptef_report(ctypes.c_char_p(status.encode('utf-8')),
-                        ctypes.c_char_p(testname.encode('utf-8')))
+    ctypes.set_errno(0)
+    rc = libptef.ptef_report(ctypes.c_char_p(status.encode('utf-8')),
+                             ctypes.c_char_p(testname.encode('utf-8')))
+    if rc == -1:
+        errno = ctypes.get_errno()
+        raise OSError(errno, os.strerror(errno), None)
 
 def mklog(testname: str) -> typing.BinaryIO:
+    ctypes.set_errno(0)
     fd = libptef.ptef_mklog(ctypes.c_char_p(testname.encode('utf-8')))
-    return os.fdopen(fd, 'wb')
+    if fd == -1:
+        errno = ctypes.get_errno()
+        raise OSError(errno, os.strerror(errno), None)
+    else:
+        return os.fdopen(fd, 'wb')
