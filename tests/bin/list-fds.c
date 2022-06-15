37188faad9fd2464b5479f59fb0fd05908e197bb
Jiri Jaburek <comps@nomail.dom>
2022-06-15 19:28:10 +0200

    don't run misc/open_fds test under valgrind
    Also rewrite list-fds because _SC_OPEN_MAX indicates the *maximum*
    number of open fds, not the maximum fd number. While these two will
    generally be the same, a parent environment can easily dup2() a fd
    above _SC_OPEN_MAX.
    
    Signed-off-by: Jiri Jaburek <comps@nomail.dom>

--- tests/bin/list-fds.c
+++ tests/bin/list-fds.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
-#include <errno.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <dirent.h>
 
 int main()
 {
-    char procpath[64], linkpath[1024];
-    long max = sysconf(_SC_OPEN_MAX);
-    for (long i = 0; i < max; i++) {
-        int ret = fcntl(i, F_GETFD);
-        if (ret == -1) {
-            if (errno == EBADF)
-                continue;
-            perror("fcntl");
-            return 1;
-        }
-        printf("%ld\n", i);
-        snprintf(procpath, sizeof(procpath), "/proc/self/fd/%ld", i);
-        ssize_t len = readlink(procpath, linkpath, sizeof(linkpath));
+    DIR *d = opendir("/proc/self/fd");
+    if (d == NULL) {
+        perror("opendir");
+        return 1;
+    }
+    int dfd = dirfd(d);
+
+    struct dirent *dent;
+    while ((dent = readdir(d)) != NULL) {
+        if (dent->d_name[0] == '.')
+            continue;
+        if (atoi(dent->d_name) == dfd)
+            continue;
+        printf("%s\n", dent->d_name);
+        char linkpath[1024];
+        ssize_t len = readlinkat(dfd, dent->d_name, linkpath, sizeof(linkpath));
         if (len != -1) {
             if (len == sizeof(linkpath))
                 len--;
             linkpath[len] = '\0';
-            fprintf(stderr, "%ld: %s\n", i, linkpath);
+            fprintf(stderr, "%s: %s\n", dent->d_name, linkpath);
         }
     }
+
+    closedir(d);
+
     return 0;
 }
